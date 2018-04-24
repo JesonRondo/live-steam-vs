eecf5264d1f1847f5880c4e4a3d028b9bc26a749
LongZhou <pancnlz@hotmail.com>
2018-04-24 12:43:16 +0800

    path fix

--- server/rtmp/index.js
+++ server/rtmp/index.js
@@ -2,9 +2,9 @@
  * 上行推流：通过 RTMP 协议把视频流推送到 rtmp://localhost/live/livestream，例如使用 ffmpeg -re -i demo.flv -c copy -f flv rtmp://localhost/live/livestream 推送；
  * 下行播放：支持以下三种播放协议，播放地址如下：
  *    RTMP: rtmp://localhost/live/livestream
- *    FLV: http://localhost:8081/live/livestream.flv
+ *    FLV: http://localhost:8181/live/livestream.flv
  */ 
 var exec = require('child_process').execSync;
 exec([
-  '/root/srs/trunk/objs/srs -c conf/http.flv.live.conf'
+  '/root/srs/trunk/objs/srs -c /root/srs/trunk/conf/http.flv.live.conf'
 ].join(' '), { stdio: [0, 1, 2]});
