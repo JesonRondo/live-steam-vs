e53422a7453a52797c6021bf91c01acda07a1375
LongZhou <pancnlz@hotmail.com>
2018-04-24 12:33:56 +0800

    http flv

--- client/end/anchor/node/rtmp.js
+++ client/end/anchor/node/rtmp.js
@@ -1,10 +1,31 @@
 // node 摄像头推流
-// for mac
+var args = require('args');
 var exec = require('child_process').execSync;
 
-exec([
-  'ffmpeg',
-  '-f avfoundation -framerate 30 -video_size 640x480 -i "0" -vcodec libx264 -preset ultrafast -acodec libfaac',
-  '-f flv',
-  'rtmp://heyai.me/live/movie'
-].join(' '), { stdio: [0, 1, 2] });
+args
+  .option('platform', 'The platform to on live. [mac, pi]', 'mac');
+
+var flags = args.parse(process.argv);
+
+// 直播端
+switch(flags.platform) {
+  // mac
+  case 'mac':
+    exec([
+      'ffmpeg',
+      '-f avfoundation -framerate 25 -video_size 640x480 -i "0" -vcodec libx264 -preset ultrafast -acodec libfaac',
+      '-f flv',
+      'rtmp://heyai.me/live/livestream'
+    ].join(' '), { stdio: [0, 1, 2] });
+    break;
+
+  // 树莓派
+  case 'pi':
+    exec([
+      'ffmpeg',
+      '-f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0',
+      '-f flv',
+      'rtmp://heyai.me/live/livestream'
+    ].join(' '), { stdio: [0, 1, 2] });
+    break;
+}
--- server/rtmp/index.js
+++ server/rtmp/index.js
@@ -1,11 +1,10 @@
 /** 
- * 上行推流：通过 RTMP 协议把视频流推送到 rtmp://localhost:1935/live/movie，例如使用 ffmpeg -re -i demo.flv -c copy -f flv rtmp://localhost:1935/live/movie 推送； 
- * 下行播放：支持以下三种播放协议，播放地址如下： 
- *    RTMP: rtmp://localhost:1935/live/movie 
- *    FLV: http://127.0.0.1:7001/live/movie.flv 
- *    HLS: http://127.0.0.1:7002/live/movie.m3u8 
+ * 上行推流：通过 RTMP 协议把视频流推送到 rtmp://localhost/live/livestream，例如使用 ffmpeg -re -i demo.flv -c copy -f flv rtmp://localhost/live/livestream 推送；
+ * 下行播放：支持以下三种播放协议，播放地址如下：
+ *    RTMP: rtmp://localhost/live/livestream
+ *    FLV: http://localhost:8081/live/livestream.flv
  */ 
-var exec = require('child_process').execSync; 
-exec([ 
-  '/data/tools/livego' 
+var exec = require('child_process').execSync;
+exec([
+  '/root/srs/trunk/objs/srs -c conf/http.flv.live.conf'
 ].join(' '), { stdio: [0, 1, 2]});
