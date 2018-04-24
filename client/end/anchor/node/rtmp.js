52da885d4abbfc0c2cf114b1362db483c942a4ad
LongZhou <pancnlz@hotmail.com>
2018-04-24 13:32:29 +0800

    http client

--- client/end/anchor/node/rtmp.js
+++ client/end/anchor/node/rtmp.js
@@ -23,7 +23,7 @@ switch(flags.platform) {
   case 'pi':
     exec([
       'ffmpeg',
-      '-f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0',
+      '-f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 -b:v 500k -c:v h264_omx -an',
       '-f flv',
       'rtmp://heyai.me/live/livestream'
     ].join(' '), { stdio: [0, 1, 2] });
